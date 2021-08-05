7c02816e0d11989812150dcf9fedc70ee25deaec
hediferchiou <88335337+hediferchiou@users.noreply.github.com>
2021-08-05 15:56:54 +0200

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def my_function(x):
+    return 5*x
+print(my_function(0))
+print(my_function(1))
+print(my_function(2))
+
+
+# In[ ]:
+
+
+
+
